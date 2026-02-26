3ec5f35d8a551adb0de2f0468e5b9d922b1a6020
aakthepaak <aakarsh3110@gmail.com>
2026-02-26 13:37:37 -0500

    added setup and src, accessed api keys

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages,setup
+
+setup(
+    name = "Medical_Chatbot",
+    version = "0.1.0",
+    author = "Aakarsh Reddy",
+    author_email = "aakarsh3110@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+
+)
